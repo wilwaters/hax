465a4c5d80bbfbfb2f3748d9fc8d71a68d86d135
Wil Waters <wil.waters@arup.com>
2021-10-19 15:36:54 +0100

    changed order of print statement for enabling editor tracking

--- atlas prep.py
+++ atlas prep.py
@@ -1,9 +1,9 @@
 # from Samual Roberts, adapted to enable editor tracking by Wilfred Waters
 # Use this script in prep for sharing on atlas (Adds global Ids & Enables editor tracking)
 print("Importing Arcpy...")
-import arcpy
-import os
+import arcpy, os
 
+# currently it uses a .SDE file stored on C:\ but you can also specify a UNC path
 SDEpath = r"C:\dev\SDEs\GBR_00000000_FuseDemo Creator.sde"
 arcpy.env.workspace = SDEpath
 
@@ -29,8 +29,8 @@ for fds in arcpy.ListDatasets("", "feature") + [""]:
             if fc_properties["editorTrackingEnabled"]:
                 print(fc + " already has editor tracking enabled")
             else:
-                print("Enabling editor tracking on " + fc)
                 arcpy.EnableEditorTracking_management(fc, "ET_CREATOR", "ET_CREATED", "ET_EDITOR", "ET_EDITED", "ADD_FIELDS", "UTC")
+                print("Enabled editor tracking on " + fc)
         except Exception as ex:
             print("Failed to enable editor tracking for " + fc)
             pass
